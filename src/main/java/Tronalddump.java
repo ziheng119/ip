558cd69f5e474868e9c3d326063d95ba4b4a666f
ziheng119 <ganziheng119@gmail.com>
2025-08-20 21:18:04 +0800

    Delete

--- src/main/java/Tronalddump.java
+++ src/main/java/Tronalddump.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,8 +8,7 @@ public class Tronalddump {
         System.out.println("GOOD MORNING! I AM TRONALD DUMP! GOD BLESS AMERICA\n"
                 + "WHAT CAN I, THIS BEAUTIFUL MAN, DO FOR YOU?\n");
         System.out.println("________________________________\n");
-        Task[] list = new Task[100];
-        int index = 0;
+        ArrayList<Task> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLine()) {
@@ -19,58 +19,66 @@ public class Tronalddump {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 System.out.print("HERE ARE THE TASKS IN YOUR LIST:\n");
-                for (int i = 0; i < index; i++) {
-                    System.out.println(String.format("%d. %s",i + 1, list[i].toString()));
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format("%d. %s",i + 1, list.get(i).toString()));
                 }
             } else if (parts.length == 2 && parts[0].equalsIgnoreCase("mark")) {
                 int taskNumber = Integer.parseInt(parts[1]) - 1;
-                if (taskNumber >= 0 && taskNumber < index) {
-                    list[taskNumber].markAsDone();
+                if (taskNumber >= 0 && taskNumber <= list.size()) {
+                    list.get(taskNumber).markAsDone();
                     System.out.println(String.format("GOOD JOB! TASK %d IS MARKED AS DONE.\n", taskNumber + 1));
-                    System.out.println(list[taskNumber]);
+                    System.out.println(list.get(taskNumber));
                 } else {
                     System.out.println("Invalid task number.");
                 }
             } else if (parts.length == 2 && parts[0].equalsIgnoreCase("unmark")) {
                 int taskNumber = Integer.parseInt(parts[1]) - 1;
-                if (taskNumber >= 0 && taskNumber < index) {
-                    list[taskNumber].markAsNotDone();
+                if (taskNumber >= 0 && taskNumber <= list.size()) {
+                    list.get(taskNumber).markAsNotDone();
                     System.out.println(String.format("TASK %d IS UNMARKED.\n", taskNumber + 1));
-                    System.out.println(list[taskNumber]);
+                    System.out.println(list.get(taskNumber));
                 } else {
                     System.out.println("Invalid task number.");
                 }
-            }
-            else {
+            } else if (parts.length == 2 && parts[0].equalsIgnoreCase("delete")) {
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                if (taskNumber >= 0 && taskNumber <= list.size()) {
+                    Task removedTask = list.remove(taskNumber);
+                    System.out.println(String.format("TASK %d HAS BEEN REMOVED:\n%s", taskNumber + 1, removedTask));
+                    System.out.println(String.format("NOW YOU HAVE %d %s IN THE LIST.\n", list.size(), list.size() == 1 ? "TASK" : "TASKS"));
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            } else {
                 if (parts[0].equalsIgnoreCase("todo")) {
                     if (parts.length == 1) {
                         System.out.println("I HATE DEMOCRATS! YOU NEED TO GIVE ME A DESCRIPTION FOR THE TODO TASK!");
                         System.out.println("________________________________\n");
                         continue;
                     }
-                    list[index++] = new Todo(input.substring(5));
+                    list.add(new Todo(input.substring(5)));
                 } else if (parts[0].equalsIgnoreCase("event")) {
                     if (parts.length == 1) {
                         System.out.println("I HATE DEMOCRATS! YOU NEED TO GIVE ME A DESCRIPTION FOR THE EVENT TASK!");
                         System.out.println("________________________________\n");
                         continue;
                     }
-                    list[index++] = new Event(input.substring(6));
+                    list.add(new Event(input.substring(6)));
                 } else if (parts[0].equalsIgnoreCase("deadline")) {
                     if (parts.length == 1) {
                         System.out.println("I HATE DEMOCRATS! YOU NEED TO GIVE ME A DESCRIPTION FOR THE DEADLINE TASK!");
                         System.out.println("________________________________\n");
                         continue;
                     }
-                    list[index++] = new Deadline(input.substring(9));
+                    list.add(new Deadline(input.substring(9)));
                 } else {
                     System.out.println("I HATE DEMOCRATS! I ONLY UNDERSTAND TODO, EVENT, AND DEADLINE TASKS!");
                     System.out.println("________________________________\n");
                     continue;
                 }
-                String formatted = String.format("OKAY. I HAVE ADDED THIS TASK:\n %s", list[index - 1].toString());
+                String formatted = String.format("OKAY. I HAVE ADDED THIS TASK:\n %s", list.get(list.size() - 1).toString());
                 System.out.println(formatted);
-                System.out.println(String.format("NOW YOU HAVE %d %s IN THE LIST.\n", index, index == 1 ? "TASK" : "TASKS"));
+                System.out.println(String.format("NOW YOU HAVE %d %s IN THE LIST.\n", list.size(), list.size() == 1 ? "TASK" : "TASKS"));
             }
             System.out.println("________________________________\n");
         }
