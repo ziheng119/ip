77203f88f6bd82d7d43ef3709712624103965ec8
ziheng119 <ganziheng119@gmail.com>
2025-08-20 12:12:52 +0800

    Mark as Done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
 }
--- src/main/java/Tronalddump.java
+++ src/main/java/Tronalddump.java
@@ -1,29 +1,55 @@
 import java.util.Scanner;
 
+
 public class Tronalddump {
     public static void main(String[] args) {
         System.out.println("________________________________\n");
         System.out.println("GOOD MORNING! I AM TRONALD DUMP! GOD BLESS AMERICA\n"
                 + "WHAT CAN I, THIS BEAUTIFUL MAN, DO FOR YOU?\n");
         System.out.println("________________________________\n");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            String[] parts = input.split(" ");
+            System.out.println("________________________________\n");
             if (input.equalsIgnoreCase("bye")) {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 for (int i = 0; i < index; i++) {
-                    System.out.println(String.format("%d. %s",i + 1,list[i]));
+                    if (list[i].isDone()) {
+                        System.out.println(String.format("%d. [X] %s",i + 1,list[i].getDescription()));
+                    } else {
+                        System.out.println(String.format("%d. [ ] %s", i + 1, list[i].getDescription()));
+                    }
                 }
-            } else {
-                list[index++] = input;
-                System.out.println("________________________________\n");
+            } else if (parts.length == 2 && parts[0].equalsIgnoreCase("mark")) {
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                if (taskNumber >= 0 && taskNumber < index) {
+                    list[taskNumber].markAsDone();
+                    System.out.println(String.format("GOOD JOB! TASK %d IS MARKED AS DONE.\n", taskNumber + 1));
+                    System.out.println(String.format("[X] %s", list[taskNumber].getDescription()));
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (parts.length == 2 && parts[0].equalsIgnoreCase("unmark")) {
+                int taskNumber = Integer.parseInt(parts[1]) - 1;
+                if (taskNumber >= 0 && taskNumber < index) {
+                    list[taskNumber].markAsNotDone();
+                    System.out.println(String.format("TASK %d IS UNMARKED.\n", taskNumber + 1));
+                    System.out.println(String.format("[ ] %s", list[taskNumber].getDescription()));
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            }
+            else {
+                list[index++] = new Task(input);
                 String formatted = String.format("YOU SAID: %s", input.toUpperCase());
                 System.out.println(formatted);
-                System.out.println("________________________________\n");
+
             }
+            System.out.println("________________________________\n");
         }
         System.out.println("THANK YOU FOR YOUR ATTENTION TO THIS MATTER!\n");
         System.out.println("________________________________\n");
