7121a8aaa85e2448e29caf6ae96dfa879f3fb2cd
ziheng119 <ganziheng119@gmail.com>
2025-08-20 11:33:02 +0800

    Add, List

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Tronalddump User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Tronalddump.java
+++ src/main/java/Tronalddump.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
-public class Jeffybezos {
+public class Tronalddump {
     public static void main(String[] args) {
         System.out.println("________________________________\n");
-        System.out.println("Hello! I'm jeffybezos\n"
-                + "What can I do for you?\n");
+        System.out.println("GOOD MORNING! I AM TRONALD DUMP! GOD BLESS AMERICA\n"
+                + "WHAT CAN I, THIS BEAUTIFUL MAN, DO FOR YOU?\n");
         System.out.println("________________________________\n");
+        String[] list = new String[100];
+        int index = 0;
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(String.format("%d. %s",i + 1,list[i]));
+                }
             } else {
+                list[index++] = input;
                 System.out.println("________________________________\n");
-                System.out.println(input);
+                String formatted = String.format("YOU SAID: %s", input.toUpperCase());
+                System.out.println(formatted);
                 System.out.println("________________________________\n");
             }
         }
